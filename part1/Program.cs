7ccef9efa6f4fd0c88e617c1801b0190cc172152
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-05-06 16:17:54 +0300

    exercise1 ok

--- part1/Program.cs
+++ part1/Program.cs
@@ -6,7 +6,10 @@ namespace part1
     {
         static void Main(string[] args)
         {
-            
+            Numbers num = new Numbers();
+            Console.WriteLine(num.Sum(4075));
+            Console.WriteLine(num.Sum(3));
+            Console.WriteLine(num.Sum(999999999));
         }
     }
 }
