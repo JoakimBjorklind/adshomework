586f586629eff77a9eab4a3d6736026360c2a6c5
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-05-15 13:07:51 +0300

    exercise1 ok

--- part2/Program.cs
+++ part2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace part2
 
@@ -7,7 +8,16 @@ namespace part2
     {
         static void Main(string[] args)
         {
+            Compare compare = new Compare();
 
+            string input10 = compare.CreateInput(10);
+            string input100 = compare.CreateInput(100);
+            string input1000 = compare.CreateInput(1000);
+            string input10000 = compare.CreateInput(10000);
+
+            compare.Case1(input10000);
+            compare.Case2(input10000);
+            
         }
     }
 
--- part2/exercise1.cs
+++ part2/exercise1.cs
@@ -6,6 +6,55 @@ namespace part2
 {
     public class Compare
     {
+        // Case1 represents the case O(n^2)
+        public int Case1(string n)
+        {
+            int counter = 0;
+
+            DateTime start1 = DateTime.Now;
+
+            for(int i = 0; i < n.Length -1; i++)
+            {
+                for(int j = 1; j < n.Length -1; j++)
+                {
+                    if (n[i].ToString() == "0" && n[j].ToString() == "1")
+                    {
+                        counter = counter + 1;
+                    }
+                }
+            }
+
+            DateTime end1 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end1.Subtract(start1));
+            return counter;
+        }
+
+        // Case2 represents the case O(n)
+        public int Case2(string n)
+        {
+            int counter = 0;
+            int zeros = 0;
+
+            DateTime start2 = DateTime.Now;
+
+            for (int i = 0; i < n.Length -1; i++)
+            {
+                if(n[i].ToString() == "0")
+                {
+                    zeros = zeros + 1;
+                }
+                else
+                {
+                    counter = counter + zeros;
+                }
+            }
+
+            DateTime end2 = DateTime.Now;
+            Console.WriteLine("Time this took: " + end2.Subtract(start2));
+            return counter;
+        }
+
+        // These exercises was run by a i3 computer.
         
         
         
@@ -23,7 +72,7 @@ namespace part2
                 sb.Append(random.Next(0, 2).ToString());
             }
 
-            return input;
+            return sb.ToString();
 
         }
     }
