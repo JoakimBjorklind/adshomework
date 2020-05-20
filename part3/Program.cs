9702816f55fafaf507c6ef80e9237f2e7891efc1
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-05-20 13:55:23 +0300

    Changed the place of the Randomizer

--- part3/Program.cs
+++ part3/Program.cs
@@ -8,7 +8,7 @@ namespace part3
         {
             PrintHello printHello = new PrintHello();
             printHello.Hello(5);
-            
+
             /*
             SmallestDifference s = new SmallestDifference();
             Console.WriteLine(s.Calculate(new int[] { 4, 1, 8, 5 })); // 1
@@ -45,6 +45,19 @@ namespace part3
             }
             */
 
+
         }
+        public static int[] Randomizer(int n)
+        {
+            Random random = new Random();
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                // integers between 1 and 1000 are enough for us
+                arr[i] = random.Next(1, 1001);
+            }
+            return arr;
+        }
+
     }
 }
--- part3/exercise3.cs
+++ part3/exercise3.cs
@@ -14,17 +14,7 @@ namespace part3
 
         }
 
-        public static int[] Randomizer(int n)
-        {
-            Random random = new Random();
-            int[] arr = new int[n];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                // integers between 1 and 1000 are enough for us
-                arr[i] = random.Next(1, 1001);
-            }
-            return arr;
-        }
+        
 
 
     }
