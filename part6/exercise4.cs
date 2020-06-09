215314646b82ef632f6d04e6d64e0acbd8b86d0d
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-06-09 20:29:14 +0300

    Built structure for part 6 exercises

---
+++ part6/exercise4.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace part6
+{
+    public class ShortestPath
+    {
+        public int n;
+        public ShortestPath(int n)
+        {
+            this.n = n;
+        }
+
+        public void AddRoad(int a, int b, int d)
+        {
+
+        }
+
+        List<int> Create(int x, int y)
+        {
+
+        }
+    }
+}
