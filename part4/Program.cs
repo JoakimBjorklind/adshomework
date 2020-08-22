2acaa834ad7cba9184097431594fa68ed0597897
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-08-22 13:30:36 +0300

    Fixed the exercises for p4 with correct solutions!

--- part4/Program.cs
+++ part4/Program.cs
@@ -7,7 +7,6 @@ namespace part4
     {
         static void Main(string[] args)
         {
-            
             LinkedList myLinks = new LinkedList();
             myLinks.AddLast(1);
             myLinks.AddFirst(2);
@@ -17,6 +16,8 @@ namespace part4
             Console.WriteLine(myLinks); // for example: 1, 3
             Console.WriteLine(myLinks.GetNode(0)); // 1
             Console.WriteLine(myLinks.GetNode(1)); // 3
+            
+            
             
             
             Console.WriteLine();
